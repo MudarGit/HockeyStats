d754afbd148126b3c20b840d1f17a8479cb089e1
MudarGit <kneehighmud@gmail.com>
2015-05-31 12:20:41 -0400

    + stats controller and view

---
+++ HockeyStats/Migrations/201505311559435_changedStats.cs
@@ -0,0 +1,16 @@
+namespace HockeyStats.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changedStats : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
