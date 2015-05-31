09f1c9ab2555b1b5524a7e9aa2655c0528cc627f
MudarGit <kneehighmud@gmail.com>
2015-05-31 11:40:34 -0400

    Initial Commit

---
+++ HockeyStats/Models/Stats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HockeyStats.Models
+{
+    public class Stats
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+        public int GamesPlayed { get; set; }
+        public int Goals { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+    }
+}
